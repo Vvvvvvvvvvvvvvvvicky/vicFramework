ff86bcee0fa9e6a9f2940f0baf6fc0195244154f
Vvvvvvvvvvvvvvvvicky <vicky.h.m.lee@outlook.com>
2019-08-11 21:59:09 +0800

    update

---
+++ src/main/java/club/vtuka/vicFramework/ConfigConstant.java
@@ -0,0 +1,15 @@
+package club.vtuka.vicFramework;
+
+public interface ConfigConstant {
+    String CONFIG_FILE = "vicFramework.properties";
+
+    String JDBC_DRIVER = "vicFramework.jdbc.driver";
+    String JDBC_URL = "vicFramework.jdbc.url";
+    String JDBC_USERNAME = "vicFramework.jdbc.username";
+    String JDBC_PASSWORD = "vicFramework.jdbc.password";
+
+    String APP_BASE_PACKAGE = "vicFramework.app.base_package";
+    String APP_JSP_PATH = "vicFramework.app.jsp_path";
+    String APP_ASSET_PATH = "vicFramework.app.asset_path";
+
+}
